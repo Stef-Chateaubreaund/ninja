b93d174a2058903ac046cc28bf533f25218d4e1b
StefannyDef <stefannydefreitas@gmail.com>
2022-07-04 16:40:33 -0400

    commt

---
+++ ninja.js
@@ -0,0 +1,20 @@
+class ninja {
+    constructor(name,health){
+        this.name = name;
+        this.health=health;
+        this.speed = 3;
+        this.strength=3;
+}
+
+sayName(){
+    console.log("${this.name}");
+}
+
+showStats(){
+    console.log("${this.name},${this.health},${this.speed},${this.strength}")
+}
+
+drinkSake(){
+    this.health += 10;
+}
+}
